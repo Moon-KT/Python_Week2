fefc17b6f25c9a06a3c54183fb9242fd0d4e73f1
Moon-KT <136817363+Moon-KT@users.noreply.github.com>
2023-11-01 15:39:57 +0700

    fix: fixed bai 4.py

--- Bai 4.py
+++ Bai 4.py
@@ -4,11 +4,16 @@ a = int(input())
 # a. Trả về số lượng các bits cần thiết để biểu diễn số a ở dạng
 # nhị phân, không bao gồm phần dấu và các số 0 ở đầu.
 
-print( bin(a)[2:])
-
+count = 0
+if a < 0:
+    a = -a
+while a > 0:
+    count += 1
+    a = a >> 1
 # b. Kiểm tra danh sách các thuộc tính và
 # phương thức hiện tại của một biến có kiểu dữ liệu là number.
 
+print(count)
 list_Methods = dir(a)
 for i in list_Methods :
     print(i)
